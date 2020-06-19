7e0ff6bcc10ac8a7571c33d28f35dfd008c7e403
mullerjones <ale.mullerjones@gmail.com>
2020-06-19 19:38:27 -0300

    Componentes aparentemente funcionando

--- ep2.cpp
+++ ep2.cpp
@@ -1,12 +1,17 @@
 #include <string>
-#include "interface.cpp"
+#include <iostream>
+#include "interface2.hpp"
 using namespace std;
 
 int main(int argc, char** argv)
 {
     int k;
+    string nomeArquivo;
     k = atoi(argv[1]);
-    Grafo *grafo = new Grafo(k);
+    nomeArquivo = argv[2];
+    Grafo *grafo = new Grafo(k, nomeArquivo);
+    int componentes = grafo->componentes();
+    printf("teste/n");
 
     return 0;
 }
